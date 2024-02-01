64cd65eee0082284785784ecdc177b334ca99fca
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-02-01 23:06:10 +0100

    ajout HeroBanner button

--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -1,17 +1,18 @@
 import Image from "next/image";
+import Right from "../icons/Right";
 
 export default function Hero() {
     return(
         <>
-            <section className="grid grid-cols-2">
-                <div>   
-                <h1 className="text-4xl font-semibold">Tout est mieux avec un TastyFastFood</h1>
+            <section className="hero">
+                <div className="py-12">   
+                <h1 className="text-4xl font-semibold">Tout est mieux avec un Fast&Tasty</h1>
                 <p className="my-4 text-gray-500">
-                    TastyFastFood est la piece manquante qui mettra du gout à votre journée
+                  <span className="text-primary">Fast&Tasty</span>est la piece manquante qui mettra du gout à votre journée
                  </p>
-                 <div className="flex gap-4">
-                    <button className="bg-primary text-white rounded-full py-2 px-8 ">Commander</button>
-                    <button className="">Découvrir</button>
+                 <div className="flex gap-4 items-center">
+                    <button className="bg-primary uppercase flex  gap-2 text-white rounded-full py-2 px-4 text-sm items-center">Commander <Right className="w-7 h-7"/></button>
+                    <button className="flex gap-2 py-2 px-4 text-gray-600 font-semibold">Découvrir <Right/></button>
                  </div>
                 </div>
                 <div className=" relative">
