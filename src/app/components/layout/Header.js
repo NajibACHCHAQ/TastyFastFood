85e7899bc1c60e3ef045dd0fc4d6064dd5bd1282
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-02-01 22:03:38 +0100

    ajout HeroBanner

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
     return(
 <>
     <header className="flex items-center justify-between">
-        <Link className="text-primary font-semibold text-2xl" href={''}>Tasty Fast Food</Link>
+        <Link className="text-primary font-semibold text-2xl" href={''}>Tasty&Fast</Link>
         <nav className="flex items-center gap-8 text-gray-500 font-semibold">
             <Link href={''}>Acceuil</Link>
             <Link href={''}>Menu</Link>
--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -1,9 +1,22 @@
+import Image from "next/image";
+
 export default function Hero() {
     return(
         <>
-            <section>
-                <h1>Tout est mieux avec un TastyFastFood</h1>
-                <p>TastyFastFood est la piece manquante qui mettra du gout à votre journée </p>
+            <section className="grid grid-cols-2">
+                <div>   
+                <h1 className="text-4xl font-semibold">Tout est mieux avec un TastyFastFood</h1>
+                <p className="my-4 text-gray-500">
+                    TastyFastFood est la piece manquante qui mettra du gout à votre journée
+                 </p>
+                 <div className="flex gap-4">
+                    <button className="bg-primary text-white rounded-full py-2 px-8 ">Commander</button>
+                    <button className="">Découvrir</button>
+                 </div>
+                </div>
+                <div className=" relative">
+                    <Image src={'/pizza.png'} alt="food" layout={'fill'} objectFit={'contain'}/>
+                </div>               
             </section>
         </>
     )
