3a3aac2e924cfab53905fe72e0d5ada708f3a785
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-02-02 09:22:49 +0100

    ajout HeroBanner button

--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -6,12 +6,13 @@ export default function Hero() {
         <>
             <section className="hero">
                 <div className="py-12">   
-                <h1 className="text-4xl font-semibold">Tout est mieux avec un Fast&Tasty</h1>
-                <p className="my-4 text-gray-500">
-                  <span className="text-primary">Fast&Tasty</span>est la piece manquante qui mettra du gout à votre journée
+                <h1 className="text-4xl font-semibold">Tout est <span className="text-primary">Meilleur</span> avec<br/> <span className="text-primary">Fast&Tasty</span></h1>
+                <p className="my-6 text-gray-500">
+                  <span className="text-primary text-xl">Fast&Tasty </span>
+                 <br/>La piece manquante qui mettra du <span className="text-primary text-xl">Gout </span> à votre journée
                  </p>
                  <div className="flex gap-4 items-center">
-                    <button className="bg-primary uppercase flex  gap-2 text-white rounded-full py-2 px-4 text-sm items-center">Commander <Right className="w-7 h-7"/></button>
+                    <button className="bg-primary uppercase flex  gap-2 text-white rounded-full py-2 px-4 text-sm items-center hover:font-semibold">Commander <Right className="w-7 h-7"/></button>
                     <button className="flex gap-2 py-2 px-4 text-gray-600 font-semibold">Découvrir <Right/></button>
                  </div>
                 </div>
@@ -19,6 +20,7 @@ export default function Hero() {
                     <Image src={'/pizza.png'} alt="food" layout={'fill'} objectFit={'contain'}/>
                 </div>               
             </section>
+        
         </>
     )
 }
